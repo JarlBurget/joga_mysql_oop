86812a426335a884bd4cf31e20c8fafb3a08a911
JarlBurget <jarl-ivor.burget@voco.ee>
2025-09-09 14:30:20 +0300

    Displayed an article by slug using the article oop model

--- routes/articles.js
+++ routes/articles.js
@@ -4,4 +4,6 @@ const articleController = require('../controllers/article')
 
 router.get('/', (req, res) => articleController.getAllArticles(req, res))
 
+router.get('/article/:slug', (req, res) => articleController.getArticleBySlug(req, res))
+
 module.exports = router
