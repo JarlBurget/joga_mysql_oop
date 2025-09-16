fdbef2da94a86fd0af5a511b2ee43b527810297c
JarlBurget <jarl-ivor.burget@voco.ee>
2025-09-16 14:57:45 +0300

    Created users table and added user data

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+//const db = require('./utils/db.js');
+//import articleRouter from './routers/article.js';
+const sessions = require('express-session');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+
+app.use(sessions({
+    secret: "thisismysecretkey",
+    saveUninitialized:true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+    resave: false 
+}));
+
+const PORT = 3000;
 app.use(express.json());
 
 const articleRouter = require('./routers/article');
-const authorRouter = require('./routers/author');
-
 app.use('/', articleRouter);
-app.use('/author/', authorRouter);
 
-app.listen(3059, () => {
-    console.log('Server running on http://localhost:3059');
+const authorRouter = require('./routers/author');
+app.use('/', authorRouter);
+
+const userRouter = require('./routers/users');
+app.use('/', userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
---
+++ models/user.js
@@ -0,0 +1,17 @@
+const BaseSQLModel = require('./base');
+
+class User extends BaseSQLModel {
+    constructor() {
+        super('users'); // MySQL tabeli nimi
+    }
+
+    async findById(id) {
+        return await super.findById(id);
+    }
+
+    async create(data) {
+        return await super.create(data);
+    }
+}
+
+module.exports = User;
